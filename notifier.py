8a75948b47151cff33348a858fb722de3b292d00
Piotr <piotr_czajka@outlook.com>
2017-03-19 22:53:35 +0100

    Notifications recievment possible

---
+++ notifier.py
@@ -0,0 +1,15 @@
+import subprocess
+
+idx = 0
+
+def get_notification():
+    global idx
+    echo = subprocess.Popen(['echo', 'GET'], stdout = subprocess.PIPE)
+    notifications = subprocess.check_output(['nc', 'localhost', '8753'], stdin=echo.stdout).decode('utf-8').splitlines()
+    if not notifications:
+        return 'No notification'
+    if not idx < len(notifications):
+        idx = 0
+    ret = notifications[idx]
+    idx += 1
+    return ret
