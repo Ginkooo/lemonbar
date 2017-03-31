483580cbbc97dfbc0a851d81060ec49a230ae8b9
Piotr <piotr_czajka@outlook.com>
2017-03-31 17:49:30 +0200

    Multiprocessing done

--- mybar.py
+++ mybar.py
@@ -1,5 +1,6 @@
 #! /bin/python
 import sys
+import time
 import os
 from utils import color, align, rand_color
 import temps
@@ -9,22 +10,38 @@ import master_volume
 import song
 import notifier
 import window_title
+from threading import Event
+from multiprocessing import Process, Manager, Event
+from ctypes import c_char
+
+
+def get_output(dic, event):
+    while True:
+        event.wait()
+        parts = [
+            color(rand_color(), 'F', temps.show_temps()),
+            color(rand_color(), 'F', master_volume.get_master_volume()),
+            color(rand_color(), 'F', song.get_song_stuff()),
+            color('#ffffff', 'F', window_title.show_title()),
+            color(rand_color(), 'F', notifier.get_notification()),
+            align('r', color('#FF00FF', 'F', clock.show_time())),
+        ]
+
+
+        output = ''
+        for part in parts:
+            output += part
+        dic['output'] = output
+
+event = Event()
+manager = Manager()
+dic = manager.dict()
+dic['output'] = 'No input...'
+t = Process(target=get_output, args=(dic,event), daemon=True)
+t.start()
 
 while True:
-    parts = [
-        color(rand_color(), 'F', temps.show_temps()),
-        color(rand_color(), 'F', master_volume.get_master_volume()),
-        color(rand_color(), 'F', song.get_song_stuff()),
-        color('#ffffff', 'F', window_title.show_title()),
-        color(rand_color(), 'F', notifier.get_notification()),
-        align('r', color('#FF00FF', 'F', clock.show_time())),
-    ]
-
-
-    output = ''
-    for part in parts:
-        output += part
-
-    print(output)
+    time.sleep(1)
+    print(dic['output'])
+    event.set()
     sys.stdout.flush()
-    os.system('sleep 1')
--- notifier.py
+++ notifier.py
@@ -1,15 +1,24 @@
 import subprocess
+import sys
 import os
+import socket
+import fcntl
 import json
+from socket import socket, AF_INET, SOCK_STREAM
 
 CONFIG_FILE = os.getenv('NOTIFY_CONFIG')
 
+def log(msg):
+    print(msg)
+    sys.stdout.flush()
+
 config = {
         'HOST': 'localhost',
-        'PORT': '8751'
+        'PORT': 8755
         }
 
 if not CONFIG_FILE:
+    print('No NOTIFY_CONFIG system variable set')
     exit()
 
 with open(CONFIG_FILE, 'r') as f:
@@ -20,15 +29,41 @@ with open(CONFIG_FILE, 'r') as f:
             continue
         line = line.rstrip()
         rec = line.split('=')
+        if rec[0] == 'PORT':
+            rec[1] = int(rec[1])
         config[rec[0]] = rec[1]
 idx = 0
 
+
+def connect(s: socket) -> None:
+    '''Reconects to server'''
+
+    s = socket(AF_INET, SOCK_STREAM)
+    s.connect((config['HOST'], config['PORT']))
+
+
+def get_from_server() -> str:
+    '''Sends GET to sever and returns response'''
+    global s
+    s.sendall(b'GET')
+    data = s.recv(1024)
+    if not data:
+        connect(s)
+    else:
+        data = data.decode('utf-8').strip().splitlines()
+        return data
+
+
 def get_notification():
     global idx
-    echo = subprocess.Popen(['echo', 'GET'], stdout = subprocess.PIPE)
+    global s
     try:
-        notifications = subprocess.check_output(['nc', config['HOST'], config['PORT']], stdin=echo.stdout).decode('utf-8').splitlines()
-    except Exception as e:
+        s = socket(AF_INET, SOCK_STREAM)
+        s.connect((config['HOST'], config['PORT']))
+        notifications = get_from_server()
+    except socket.error as e:
+        sys.stderr.write(e.__traceback__)
+        sys.stderr.flush()
         return 'No connection'
     if not notifications:
         return 'No notification'
@@ -36,4 +71,4 @@ def get_notification():
         idx = 0
     ret = notifications[idx]
     idx += 1
-    return ret.replace('\n', '').replace('\r', '') + ' | '
+    return ret.rstrip() + ' | '
